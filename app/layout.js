2e04f320257f7242c1487282ce245870a9e476ce
Hamim <hamimc232@gmail.com>
2025-04-11 17:14:08 -0400

    Installed Stack Auth for Authentication and users

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,6 @@
 import { Inter } from "next/font/google";
+import { StackProvider, StackTheme } from "@stackframe/stack";
+import { stackServerApp } from "../stack";
 import "./globals.css";
 import AppTheme from "./shared-theme/AppTheme"; // Import the AppTheme component
 
@@ -13,11 +15,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="__variable_835dce" cz-shortcut-listen="true">
+      <body className="__variable_835dce" cz-shortcut-listen="true"><StackProvider app={stackServerApp}><StackTheme>
         <AppTheme> {/* Wrap children with AppTheme */}
           {children}
         </AppTheme>
-      </body>
+      </StackTheme></StackProvider></body>
     </html>
   );
 }
