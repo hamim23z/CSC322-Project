849f379b0971a2c4ef0d327b8b13caba2b5e6553
Hamim <hamimc232@gmail.com>
2025-04-11 17:11:39 -0400

    Fixed hero section. Put temporary image

--- app/home-page/components/Hero.js
+++ app/home-page/components/Hero.js
@@ -22,7 +22,7 @@ const StyledBox = styled('div')(({ theme }) => ({
   border: '1px solid',
   borderColor: (theme.vars || theme).palette.grey[200],
   boxShadow: '0 0 12px 8px hsla(220, 25%, 80%, 0.2)',
-  backgroundImage: `url(${process.env.TEMPLATE_IMAGE_URL || 'https://mui.com'}/static/screenshots/material-ui/getting-started/templates/dashboard.jpg)`,
+  backgroundImage: `url('/homepage_image.png')`,
   backgroundSize: 'cover',
   [theme.breakpoints.up('sm')]: {
     marginTop: theme.spacing(10),
@@ -30,7 +30,7 @@ const StyledBox = styled('div')(({ theme }) => ({
   },
   ...theme.applyStyles('dark', {
     boxShadow: '0 0 24px 12px hsla(210, 100%, 25%, 0.2)',
-    backgroundImage: `url(${process.env.TEMPLATE_IMAGE_URL || 'https://mui.com'}/static/screenshots/material-ui/getting-started/templates/dashboard-dark.jpg)`,
+    backgroundImage: `url('/homepage_image.png')`,
     outlineColor: 'hsla(220, 20%, 42%, 0.1)',
     borderColor: (theme.vars || theme).palette.grey[700],
   }),
@@ -74,7 +74,7 @@ export default function Hero() {
               fontSize: 'clamp(3rem, 10vw, 3.5rem)',
             }}
           >
-            Our&nbsp;latest&nbsp;
+            Smart
             <Typography
               component="span"
               variant="h1"
@@ -86,7 +86,7 @@ export default function Hero() {
                 }),
               })}
             >
-              products
+              &nbsp;Finder4U
             </Typography>
           </Typography>
           <Typography
@@ -96,9 +96,7 @@ export default function Hero() {
               width: { sm: '100%', md: '80%' },
             }}
           >
-            Explore our cutting-edge dashboard, delivering high-quality solutions
-            tailored to your needs. Elevate your experience with top-tier features
-            and services.
+            LLM. DESCRIPTION OF PRODUCT GOES HERE
           </Typography>
           <Stack
             direction={{ xs: 'column', sm: 'row' }}
@@ -142,7 +140,7 @@ export default function Hero() {
             <Link href="#" color="primary">
               Terms & Conditions
             </Link>
-            .
+            . And join the waitlist
           </Typography>
         </Stack>
         <StyledBox id="image" />
